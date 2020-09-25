92b4609e24f64e1cb1c6e3705e41bced7d6f669c
Erasmo <erasmo.cjunior@gmail.com>
2020-09-25 10:42:14 -0300

    Altera bot

--- comAltera_bot/echo_bot.py
+++ comAltera_bot/echo_bot.py
@@ -1,7 +1,23 @@
-import telebot
+print("Ola, quem tai?")
+nome = input(">: ")
 
-bot = telebot.TeleBot("")
+if "O meu nome eh" in nome:
+    nome = nome[14:]
 
-@bot.message_handler(commands=['start','help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+conhecidos = ['Raimundo', 'Will', 'Joao' ]
+frase = 'Muito prazer '
+if nome in conhecidos:
+    frase = 'Eaew'
+else:
+    frase = 'Muito prazer '
+
+print(frase + nome)
+
+while True:
+    resposta = input(">: ")
+    if resposta == "tchau":
+        break
+    else:
+        print("Digite outra coisa")
+
+print("Tchau, bye!")
